d2aa355902ce8822ddb384811cf74f408364fac5
PravinkumarRD <dabade.pravinkumar@gmail.com>
2024-12-05 16:06:39 +0530

    Delete Angular/bajaj-events-portal directory

--- Angular/bajaj-events-portal/src/app/statics/employee-statics.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EmployeeStatics } from './employee-statics';
-
-describe('EmployeeStatics', () => {
-  it('should create an instance', () => {
-    expect(new EmployeeStatics()).toBeTruthy();
-  });
-});
--- Angular/bajaj-events-portal/src/app/statics/event-statics.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EventStatics } from './event-statics';
-
-describe('EventStatics', () => {
-  it('should create an instance', () => {
-    expect(new EventStatics()).toBeTruthy();
-  });
-});
